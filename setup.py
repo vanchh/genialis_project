5b7561f41adb3d4514af5990c8ccc6da4dfa0eab
anjaVrec <anja@predictleads.com>
2024-02-18 13:11:50 +0100

    Automatically install requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name="progeny",
     version="0.1",
-    author="Anja Vrecer",
+    author="Anja Vrečer",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
             'progeny = progeny.progeny:main'
         ]
-    }
+    },
+    install_requires=requirements
 )
